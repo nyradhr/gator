1033f56b8ffa407e4bdd390c3b8bff9d57dd9928
nyradhr <alessio.piccione@hotmail.it>
2025-08-26 10:40:09 +0200

    db refactor

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -3,8 +3,8 @@ CREATE TABLE feeds(
     id uuid PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    name VARCHAR NOT NULL,
-    url VARCHAR UNIQUE NOT NULL,
+    name TEXT NOT NULL,
+    url TEXT UNIQUE NOT NULL,
     user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
